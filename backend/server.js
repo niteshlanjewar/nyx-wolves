c56a57f8c9304280318b643e183fca14d4dcde37
niteshlanjewar <nitlanjewar1621@gmail.com>
2023-06-04 16:03:30 +0530

    chatApp7

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+
+connectDB();
+const app = express();
+
 
 app.get('/', (req, res) => {
 res.send("API is running successfully");
@@ -21,4 +26,4 @@ app.get('/api/chat/:id', (req,res) =>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`server started on PORT ${PORT}`));
+app.listen(5000, console.log(`server started on PORT ${PORT}`.yellow.bold));
